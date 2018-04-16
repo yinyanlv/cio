89292eb9e868986a8ad7e433e533d535549aa5f0
yinyanlv <yanlv.yin@gmail.com>
2018-04-16 13:16:06 +0800

    web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    for stream in listener.incoming() {
+
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "resources/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "resources/404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut body = String::new();
+
+    file.read_to_string(&mut body).unwrap();
+
+    let res = format!("{}{}", status_line, body);
+
+    stream.write(res.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
